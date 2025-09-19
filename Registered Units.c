d6708c5f014df19fc225df9e277644db800e4862
Simon92coder <s45174925@gmail.com>
2025-09-19 21:15:27 +0300

    Add files via upload

---
+++ Registered Units.c
@@ -0,0 +1,31 @@
+/*
+Name: Simon Mwangi Wanjiru
+Reg No: CT101/G/26438/25.
+Description: Program to display units taken.
+*/
+
+#include <stdio.h>
+
+
+int main(){
+	
+	
+	printf("Name:Simon Mwangi.\n");
+	printf("Registration Number: CT101/G/26438/25\n");
+	
+	// Units Registered.
+	printf("Course: BSc Computer Science.\n");
+	printf("Units registered: \n");
+	printf("UCU2105: Fundamentals of ICT.\n");
+	printf("SPC2102: Introductuon to Computer Architecture.\n");
+	printf("UCU2101: Communication skills.\n");
+	printf("SPH2107: Analogue Electronics.\n");
+	printf("SPM2100: Discrete Mathematics.\n");
+	printf("SPM2104: Mathematics for Science.\n");
+	printf("SPH2101: Physics.\n");
+	printf("SPC2101: Introduction to Programming and problem solving.\n");
+	
+	return 0;
+	
+	
+}
