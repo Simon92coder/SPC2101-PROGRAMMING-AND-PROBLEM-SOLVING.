c88e1f7094f4967de0747171a2bfcc39c2ede3e3
Simon92coder <s45174925@gmail.com>
2025-09-24 23:48:34 +0300

    Update Hello World.c

--- Hello World.c
+++ Hello World.c
@@ -3,10 +3,12 @@ Name:Simon mwangi Wanjiru.
 Reg No: CT101/G/26438/25.
 Description: Simple c program.
 */
+//pre processor directive
 #include <stdio.h>
 int main(){
 
 printf("Hello world");
 
 return 0;
+
 }
